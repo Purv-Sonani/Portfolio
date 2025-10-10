64ee3aa0b7f9529e17fd42bc90d421522eeff577
Purv-Sonani <purv360@gmail.com>
2025-10-10 14:39:01 +0200

    Date Selection change in Experience - models.py

--- home/models.py
+++ home/models.py
@@ -71,7 +71,7 @@ class Experience(models.Model):
     company = models.CharField(max_length=200)
     role = models.CharField(max_length=200)
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
     description = models.TextField(blank=True, null=True)
 
     class Meta:
