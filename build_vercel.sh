0f7bae3f2ced30025d69faed0b9eadcb73333276
Purv-Sonani <purv360@gmail.com>
2025-10-26 17:36:52 +0100

    Configure buildCommand in vercel.json for collectstatic

--- build_vercel.sh
+++ build_vercel.sh
@@ -3,5 +3,8 @@
 # Exit on error
 set -o errexit
 
-# Collect static files (make sure python3 is correct)
+# Install dependencies (Vercel might do this automatically, but explicit is safer)
+pip install -r requirements.txt
+
+# Collect static files into the 'staticfiles' directory
 python3 manage.py collectstatic --noinput --clear
