83fe7afe783773bc76e1b0369730d00e35d0b69c
Purv-Sonani <purv360@gmail.com>
2025-10-26 17:50:48 +0100

    Adjust static file output to 'public' for Vercel

--- build_vercel.sh
+++ build_vercel.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+# Exit on error
 set -o errexit
+
+# Upgrade pip (using python3 module execution)
 python3 -m pip install --upgrade pip
+
+# Install dependencies (using python3 module execution)
 python3 -m pip install -r requirements.txt
+
+# Collect static files into the directory specified by STATIC_ROOT (which is 'public')
 python3 manage.py collectstatic --noinput --clear
-mv staticfiles public
